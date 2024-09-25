01886ae94001b53d281207b9a7813dee12e9c4b7
kumargoud <desham.kumargoud1994@gmail.com>
2024-09-25 11:54:48 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "vpc_cidr" {
+    #default = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+    default = true
+}
+
+variable "common_tags" {
+    default = {}
+}
+
+variable "vpc_tags" {
+    default = {}
+}

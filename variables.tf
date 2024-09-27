00e7f300cd1b4ec47979551b63dfd5a6c75d5a87
kumargoud <desham.kumargoud1994@gmail.com>
2024-09-27 10:06:59 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -22,3 +22,31 @@ variable "common_tags" {
 variable "vpc_tags" {
     default = {}
 }
+
+variable "igw_tags" {
+    default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.public_subnet_cidrs) == 2
+        error_message = "please provide 2 valid public subnet CIDR"
+    }
+}
+
+variable "private_subnet_tags" {
+    default = {}
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.private_subnet_cidrs) == 2
+        error_message = "please provide 2 valid public subnet CIDR"
+    }
+}
+
+variable "private_subnet_tags" {
+    default = {}
+}
